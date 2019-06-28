da1b657c5f131e6c18a0cb1654da6d54d4895cc7
Kevin <jimanwen@126.com>
2019-06-28 13:53:00 +0800

    Modify View.kt

--- kotlinx/src/main/java/org/kevin/kotlinx/View.kt
+++ kotlinx/src/main/java/org/kevin/kotlinx/View.kt
@@ -3,19 +3,19 @@ package org.kevin.kotlinx
 import android.app.TimePickerDialog
 import android.content.Context
 import android.view.View
-import android.view.ViewGroup
-import android.widget.Adapter
-import android.widget.AdapterView
 import android.widget.CompoundButton
 import android.widget.TimePicker
 
-fun View.click(action: (View) -> Unit) = setOnClickListener(action)
+inline fun <reified T : View> T.onClick(noinline action: T.() -> Unit) = setOnClickListener { action(it as T) }
 
-fun View.longClick(action: (View) -> Boolean) = setOnLongClickListener(action)
+inline fun <reified T : View> T.onLongClick(noinline action: T.() -> Boolean) =
+    setOnLongClickListener { action(it as T) }
 
 fun CompoundButton.checkedChange(action: (buttonView: CompoundButton, isChecked: Boolean) -> Unit) =
     setOnCheckedChangeListener(action)
 
-fun showTimePicker(context: Context, hourOfDay: Int, minute: Int, is24Hour: Boolean,
-    action: (view: TimePicker, hourOfDay: Int, minute: Int) -> Unit) =
+fun showTimePicker(
+    context: Context, hourOfDay: Int, minute: Int, is24Hour: Boolean,
+    action: (view: TimePicker, hourOfDay: Int, minute: Int) -> Unit
+) =
     TimePickerDialog(context, TimePickerDialog.OnTimeSetListener(action), hourOfDay, minute, is24Hour).show()
